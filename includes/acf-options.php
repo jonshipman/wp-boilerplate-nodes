4d3cf5b286725f1e5f5fb8a47f06d6e712547c1b
Jon Shipman <jon@jonshipman.com>
2020-09-11 11:07:59 -0500

    catch string literals

--- includes/acf-options.php
+++ includes/acf-options.php
@@ -8,7 +8,7 @@
 // Hide the ACF menu when not debugging.
 if ( ! function_exists( 'wp_boilerplate_nodes_hide_acf_menu' ) ) {
 	function wp_boilerplate_nodes_hide_acf_menu( $bool ) {
-		if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+		if ( defined( 'WP_DEBUG' ) && ( WP_DEBUG || WP_DEBUG === 'false' ) ) {
 			return true;
 		}
 
